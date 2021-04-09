15fc818b996409cd61509cf4993f699c6cfa2d61
akshat55 <akshat@live.ca>
2021-04-08 20:45:44 -0400

    Add camera calibration to correct images

--- aruco_finder.py
+++ aruco_finder.py
@@ -1,31 +1,55 @@
 import cv2
 import numpy as np
 import cv2.aruco as aruco
-import object_size
-import transform
 
-def findAruco(img):
-    aruco_list = {}
 
+def find_aruco_marker(img):
     aruco_dict = aruco.Dictionary_get(aruco.DICT_ARUCO_ORIGINAL)
 
     # Marker detection parameters
     parameters = aruco.DetectorParameters_create()
 
-    # lists of ids and the corners belonging to each id
-    corners, ids, rejectedImgPoints = aruco.detectMarkers(img, aruco_dict, parameters=parameters)
+    # Lists of ids and the corners belonging to each id
+    corners, ids, rejected_points = aruco.detectMarkers(img, aruco_dict, parameters=parameters)
 
-    # Can subtract # of pixels to determine how long 5 cm is and use it to ratio
-    print(corners)
+    # Can subtract # of pixels to determine how long 8 cm is and use it to ratio
+    print("Corners", corners)
 
     image = img.copy()
     aruco.drawDetectedMarkers(image, corners)
-    # cv2.circle(image, (corners[0][0][3][0],corners[0][0][3][1]), 15, (0, 255, 0), -1)
-    cv2.imshow("name", image)
+
+    cv2.imshow("Marker", image)
 
     return corners[0][0]
 
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
 
+def get_calibration_data():
+    """
+    The distortion coefficient retrieved is for a specific camera; images taken on another camera may produce different results.
+    """
+    # Read Yaml file and retrieve distortion coefficient and camera matrix
+    calibration_file = cv2.FileStorage("./calibration.yaml", cv2.FileStorage_READ)
+    file_node = calibration_file.getNode('camera_matrix')
+    matrix = np.asarray(file_node.mat())
+    file_node = calibration_file.getNode('dist_coeff')
+    distortion = np.asarray(file_node.mat())
+
+    return matrix, distortion
+
+
+def calibrate_image(img, matrix, distortion, new_width=1280, new_height=960):
+
+    new_img = cv2.resize(img, (new_width, new_height))
+
+    # Get height/width of image
+    h, w = new_img.shape[:2]
+
+    new_camera_mtx, roi = cv2.getOptimalNewCameraMatrix(matrix, distortion, (w, h), 0.8, (w, h))
+    undistorted_img = cv2.undistort(new_img, matrix, distortion, None, new_camera_mtx)
+
+    # Crop the image to remove the black border (warped distortion)
+    # x, y, width, height = roi
+    # cropped_img = undistorted_img[y:y+height, x:x+width]
+
+    return undistorted_img
 
--- main.py
+++ main.py
@@ -1,22 +1,24 @@
 import cv2
-import numpy as np
-import cv2.aruco as aruco
 import object_size
 import transform
 import aruco_finder
 from scipy.spatial import distance as dist
 
 if __name__ == '__main__':
-    img = cv2.imread('./input/Mtest2.jpg', cv2.COLOR_BGR2GRAY)
-    marker = aruco_finder.findAruco(img)
+    img = cv2.imread('./input/Mtest1.jpg', cv2.COLOR_BGR2GRAY)
 
-    trans_image = transform.four_point_transform(img, marker)
+    matrix, distortion = aruco_finder.get_calibration_data()
+    calibrated_img = aruco_finder.calibrate_image(img, matrix, distortion)
+
+    marker = aruco_finder.find_aruco_marker(calibrated_img)
+
+    trans_image = transform.four_point_transform(calibrated_img, marker)
     cv2.imshow("transformed", trans_image)
     cv2.imwrite("./output/transformed.jpg", trans_image)
 
-    trans_marker = aruco_finder.findAruco(trans_image)
+    trans_marker = aruco_finder.find_aruco_marker(trans_image)
     pixelsPerMetric = dist.euclidean(trans_marker[2], trans_marker[1]) / 8
-    print(pixelsPerMetric)
+    print("PPM: ", pixelsPerMetric)
 
     # pass the rotated image as well as the bottom right coordinates of the aruco marker
     object_size.objectsize(trans_image, trans_marker[0], pixelsPerMetric)
